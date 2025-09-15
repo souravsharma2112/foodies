f4f9f50850d518d1bb32717a6ea457daac3325d7
Sourav Kumar <souravsharma2102@gmail.com>
2025-09-15 23:44:44 +0530

    helper files updated

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [[
+    'module-resolver',
+    {
+      root: ['./src'],
+      alias : {
+        '@assets' : './src/assets',
+        '@utils': './src/utils'
+      }
+    }
+  ]]
 };
